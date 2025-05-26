e4827c490107237d84c53139cbfea11847d7f3b9
rajkumar <rrajkumarr104@gmail.com>
2025-05-26 21:49:35 +0530

    Implemented Complete and Exit on GUI

--- gui.py
+++ gui.py
@@ -1,14 +1,27 @@
 from modules import functions as fn
 import FreeSimpleGUI as sg
 
+# Left Side Column
 label = sg.Text("Type In A To-Do")
 input_box = sg.InputText(tooltip="Enter To-Do",key="todo")
-add_button = sg.Button("Add")
 list_box = sg.Listbox(values=fn.get_todos(), key="todos",
                       enable_events=True, size=[45,10])
-edit_button = sg.Button("Edit")
+output_text = sg.Text(key="output")
+left_column =sg.Column([[label],[input_box],[list_box],[output_text]])
 
-Window = sg.Window("My To-do App",layout=[[label, add_button],[input_box],[list_box,edit_button]],
+
+# Right Side Column
+add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+right_column =sg.Column([[add_button],[edit_button],[complete_button],[exit_button]])
+
+# Construct the layout
+layout = [[left_column, right_column]]
+
+
+Window = sg.Window("My To-do App",layout=layout,
                    font=('Comic Sans MS',20))
 while True:
     event, values = Window.read()
@@ -18,6 +31,7 @@ while True:
             todos = fn.get_todos()
             todos.append(values["todo"] + "\n")
             fn.set_todos(todos)
+            Window["todos"].update(values=todos)
 
         case "Edit":
             todos = fn.get_todos()
@@ -30,7 +44,17 @@ while True:
         case "todos":
             Window["todo"].update(value=values['todos'][0])
 
-        case sg.WIN_CLOSED:
+        case "Complete":
+            todos = fn.get_todos()
+            to_complete = values['todos'][0]
+            index = todos.index(to_complete)
+            completed = todos.pop(index).strip("\n")
+            Window["output"].update(value=f"`{completed}` is marked completed")
+            fn.set_todos(todos)
+            Window["todos"].update(values=todos)
+            Window["todo"].update(value="")
+
+        case sg.WIN_CLOSED | "Exit":
             break
 
 Window.close()
